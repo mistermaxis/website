94a51c443b136a3514009a3aa0f060bb4494d0b6
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2023-03-01 18:18:58 -0300

    Add home component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Home from './Pages/Home.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + Crack Cocaine</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="container flex flex-col items-center pt-5 pb-5">
+     <Home />
+     
     </div>
   )
 }
