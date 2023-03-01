e93de11237cf7b6b714b3b3566399843577d198d
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2023-03-01 18:20:13 -0300

    Setup basic home page

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,19 @@
+const Home = () => (
+  <>
+    <h1 className="bg-red-500 text-4xl font-bold">Agrimensura</h1>
+    <article className="w-11/12 sm:w-8/12 text-xl sm:text-2xl text-center">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat dolore qui optio quae, amet repudiandae. Saepe est, quisquam explicabo minima unde facilis officiis recusandae accusamus quod! Quo quis aspernatur magnam.
+    </article>
+    <img className="w-11/12 sm:w-8/12 text-xl" src="images/photo_01.jpeg" alt="First Picture" />
+    <article className="w-11/12 sm:w-8/12 text-xl sm:text-2xl text-center">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in exercitationem et culpa animi autem officia odio amet, dolores ab ipsam suscipit necessitatibus veritatis temporibus reiciendis. Voluptatibus aut neque autem?
+    </article>
+    <img className="w-11/12 sm:w-8/12" src="images/photo_02.jpeg" alt="Second Picture" />
+    <article className="w-11/12 sm:w-8/12 text-xl sm:text-2xl text-center">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic adipisci rem odit modi, natus a veniam distinctio aspernatur minus quo, amet recusandae veritatis suscipit, ex sunt. Placeat delectus nisi unde!
+    </article>
+    <img className="w-11/12 sm:w-8/12" src="images/photo_03.jpeg" alt="Third Picture" />
+  </>
+);
+ 
+export default Home;
